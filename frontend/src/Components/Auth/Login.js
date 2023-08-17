870afb192de8466b3b5fdc459de002f108c369de
emmanuelbed <bedzoemmanuel8@gmail.com>
2023-08-16 19:02:47 -0500

    Adding the Login Auth Page

---
+++ frontend/src/Components/Auth/Login.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const Login = () => {
+  const history = useHistory();
+
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Make an API call to your backend for login
+      const response = await fetch("/api/v1/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        // Successful login, redirect to dashboard or main app
+        history.push("/dashboard"); // Update with your route
+      } else {
+        const data = await response.json();
+        console.log(data.message); // Display error message
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
