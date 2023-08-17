2b27bad1266e3e9887b9eb99911f6dda8e5f219f
emmanuelbed <bedzoemmanuel8@gmail.com>
2023-08-16 19:03:36 -0500

    Adding the Sign-up Page

---
+++ frontend/src/Components/Auth/Signup.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const Signup = () => {
+  const history = useHistory();
+
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Make an API call to your backend for signup
+      const response = await fetch("/api/v1/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        // Successful signup, redirect to login or main app
+        history.push("/login"); // Update with your route
+      } else {
+        const data = await response.json();
+        console.log(data.message); // Display error message
+      }
+    } catch (error) {
+      console.error("Error during signup:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
